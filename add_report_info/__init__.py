69c2b6f4da4ecf43f60741986200c2cd556620fa
chenditc <chenditc1@qq.com>
2021-10-14 09:55:42 +0800

    Add table access

--- add_report_info/__init__.py
+++ add_report_info/__init__.py
@@ -1,19 +1,34 @@
 import logging
-
+import os
 import azure.functions as func
+import uuid
 
+from azure.cosmosdb.table.tableservice import TableService
+# https://docs.microsoft.com/en-us/azure/cosmos-db/table/how-to-use-python
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    user_id = req.params.get("user");
+    report_json = req.params.get("report_json");
+    try:
+        req_body = req.get_json()
+    except ValueError:
+        pass
+    else:
+        report_json = req_body.get('report_json')
+        user_id = req_body.get('user_id')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-    
+    if user_id is None or report_json is None:
+        return func.HttpResponse(f"Failed to pase body")
+
+    report_id = str(uuid.uuid4())
+
+    conn_str = os.getenv("TABLE_CONNECTION_STRING")
+    table_service = TableService(connection_string=conn_str)
+    entity = {
+        'PartitionKey': user_id, 
+        'RowKey': report_id,
+        "report_json": report_json
+    }
+    table_service.insert_entity('TrafficReportInfo', entity)
 
     return func.HttpResponse(f"ok")
--- list_report_info/__init__.py
+++ list_report_info/__init__.py
@@ -1,30 +1,32 @@
 import logging
 import json
+import os
 import azure.functions as func
 
+from azure.cosmosdb.table.tableservice import TableService
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    result = [
-        {
-            "time":1633985212,
-            "lat":31.133145,
-            "lon":121.41439,
-            "video_info":{
-                "video":{
-                    "url":"http://1300035106.vod2.myqcloud.com/83d79a75vodcq1300035106/b325aabd3701925925849484897/Syev6dNTBc8A.mp4",
-                    "verify_content":"SVvY/xzSigLy7oqVGAWaNqaVSvRFeHBUaW1lPTE2MzQxMjAyNDYmRmlsZUlkPTM3MDE5MjU5MjU4NDk0ODQ4OTc="
-                },
-                "cover":{
-                    "url":"http://1300035106.vod2.myqcloud.com/83d79a75vodcq1300035106/b325aabd3701925925849484897/3701925925849484898.jpg",
-                    "verify_content":"DpKdNdg8DtjETrBhFFcOr7rsAcJFeHBUaW1lPTE2MzQxMjAyNDYmRmlsZUlkPTM3MDE5MjU5MjU4NDk0ODQ4OTg="
-                },
-                "fileId":"3701925925849484897"
-            },
-            "loc_desp":"虹梅南路 上海市徐汇区启新小学 西",
-            "time_str":"2021-10-12 04:46:52",
-            "plate_num":"沪D12345",
-            "plate_color":0
-        }
-    ]
+    user_id = req.params.get("user");
+    if user_id is None:
+        return func.HttpResponse("No user specified")
+    conn_str = os.getenv("TABLE_CONNECTION_STRING")
+    table_service = TableService(connection_string=conn_str)
+    report_list = table_service.query_entities(
+        'TrafficReportInfo', 
+        filter="PartitionKey eq '" + user_id + "'")
+
+    result = []
+    for report in report_list:
+        try:
+            report_info = json.loads(report["report_json"])
+            result.append(report_info)
+        except Exception as e:
+            print(e)
+            # If failed to parse, just delete it
+            table_service.delete_entity('TrafficReportInfo', report["PartitionKey"], report["RowKey"])
+            pass
+
     return func.HttpResponse(
         json.dumps(result),
         mimetype="application/json"
