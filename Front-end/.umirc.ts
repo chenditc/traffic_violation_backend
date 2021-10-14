c348f04e75211b5448410cee932534c8e8e31c19
Hanxian Wang <hanxianwang@microsoft.com>
2021-10-14 17:40:46 +0800

    Add front-end code.

---
+++ Front-end/.umirc.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    { path: '/', redirect: "/login" },
+    { path: "/login", component: "@/pages/Login.tsx"},
+    { path: '/list', component: "@/pages/EventList.tsx"},
+    { path: '/report', component: "@/pages/Report.tsx"}
+  ],
+  fastRefresh: {},
+  styles: ["#root {height: 100%}"],
+});
