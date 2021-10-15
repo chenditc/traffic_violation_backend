d29c66cb25014aa9a4dabc6e0a1568ab34af6730
Yajuju <caontmdb@qq.com>
2021-10-15 08:56:32 +0800

    Some UI changes.

--- Front-end/src/models/event-list.ts
+++ Front-end/src/models/event-list.ts
@@ -1,42 +1,45 @@
 import { useCallback, useState } from "react";
-import {request} from "umi";
+import { request } from "umi";
 interface IMedia {
-    url: string;
-    verify_content: string;
+  url: string;
+  verify_content: string;
 }
 interface IVideoInfo {
-    cover: IMedia;
-    fileId: string;
-    video: IMedia;
+  cover: IMedia;
+  fileId: string;
+  video: IMedia;
 }
 export interface IEvent {
-    lat: number;
-    lon: number;
-    loc_desp: string;
-    report_id: string;
-    tel: string;
-    time: number;
-    time_str: string;
-    user_id: string;
-    video_info: IVideoInfo; 
-    plate_color?: number;
-    plate_num?: string;
-    violation_type?: string;
+  lat: number;
+  lon: number;
+  loc_desp: string;
+  report_id: string;
+  tel: string;
+  time: number;
+  time_str: string;
+  user_id: string;
+  video_info: IVideoInfo;
+  plate_color?: number;
+  plate_num?: string;
+  violation_type?: string;
+  report_success?: boolean;
 }
 
-
 export default function useEventListModel() {
-    const [eventList, setEventList] = useState<IEvent[]>([]);
-    const fetchList = useCallback((userId) => {
-        request("https://traffic-violation.azurewebsites.net/api/list_report_info", {
-            method: "get",
-            params: {user: userId}
-        }).then((val) => {
-            setEventList(val);
-        });
-    },[])
-    return {
-        eventList,
-        fetchList
-    }
+  const [eventList, setEventList] = useState<IEvent[]>([]);
+  const fetchList = useCallback((userId) => {
+    request(
+      "https://traffic-violation.azurewebsites.net/api/list_report_info",
+      {
+        method: "get",
+        params: { user: userId },
+      },
+    ).then((val) => {
+      setEventList(val);
+    });
+  }, []);
+  return {
+    eventList,
+    fetchList,
+  };
 }
--- Front-end/src/pages/EventList.tsx
+++ Front-end/src/pages/EventList.tsx
@@ -1,38 +1,51 @@
-import { Card, WingBlank, WhiteSpace } from "antd-mobile";
+import { Card, Space } from "antd-mobile";
 import { useMemo, useEffect, useCallback } from "react";
 import { useModel, history } from "umi";
 import styles from "@/pages/index.less";
 import { IEvent } from "@/models/event-list";
-export default function EventListPage () {
+import {
+  CheckCircleOutline,
+  ExclamationCircleOutline,
+} from "antd-mobile-icons";
+export default function EventListPage() {
   const { eventList, fetchList } = useModel("event-list");
   useEffect(() => {
     fetchList("17602144419");
   }, []);
   const onClickItem = useCallback(
     (reportId) => () => {
-      history.push(`/report?id=${reportId}`)
+      history.push(`/report?id=${reportId}`);
     },
     [history],
-  )
+  );
   const eventCardList = useMemo(() => {
     return eventList.map((item: IEvent) => {
       return (
-        <div key={item.report_id}>
-        <WhiteSpace size="lg"/>
-        <Card onClick={onClickItem(item.report_id)}>
-          <Card.Header title={item.loc_desp}/>
-          <Card.Body>
-            <img src={item.video_info.cover.url} className={styles.cover}/>
-          </Card.Body>
-          <Card.Footer content={item.time_str}/>
+        <Card
+          onClick={onClickItem(item.report_id)}
+          title={item.loc_desp}
+          className={styles["event-card"]}
+          headerClassName={styles["event-card-title"]}
+        >
+          <img src={item.video_info.cover.url} className={styles.cover} />
+          <div className={styles["card-footer"]}>
+            <span className={styles["time-str"]}>{item.time_str}</span>
+            <span>
+              {item.report_success ? (
+                <CheckCircleOutline />
+              ) : (
+                <ExclamationCircleOutline />
+              )}
+            </span>
+            {/* TODO footer部分的垂直居中对齐；icon的颜色区分，文字说明 */}
+          </div>
         </Card>
-        </div>
-      )
-    })
-  }, [eventList])
-    return (
-        <WingBlank size="lg">
-        {eventCardList}
-        </WingBlank>
-    )
-};
+      );
+    });
+  }, [eventList]);
+  return (
+    <Space direction="vertical" className={styles["event-list"]}>
+      {eventCardList}
+    </Space>
+  );
+}
