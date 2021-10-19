156e31d941773ec007c846d90317550d547a8469
Di <chenditc@umich.edu>
2021-10-19 21:49:36 +0800

    Update Report.tsx

--- Front-end/src/pages/Report.tsx
+++ Front-end/src/pages/Report.tsx
@@ -121,7 +121,7 @@ export default function ReportPage() {
           </a>
         </div>
         <Item title="视频地址"><a href={reportInfo?.video_info.video.url} target="_blank">点击播放</a></Item>
-        <Item title="地点">{reportInfo?.loc_desp}</Item>
+        <Item title="地点">{reportInfo?.loc_response.GaoDeMiaoShu}</Item>
         <Item title="时间">{reportInfo?.time_str}</Item>
         <Item title="手机号">{reportInfo?.tel}</Item>
       </List>
