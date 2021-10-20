044ad10f8c87f7c752b718b6fd5b248c4fea7de1
Di <chenditc@umich.edu>
2021-10-20 14:01:13 +0800

    Update __init__.py

--- list_report_info/__init__.py
+++ list_report_info/__init__.py
@@ -3,9 +3,11 @@ import json
 import os
 import azure.functions as func
 from storage_utils import report_info_utils
+from report_utils import enrich_report_info
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     user_id = req.params.get("user");
+    refresh_info = req.params.get("refresh_info");
     if user_id is None:
         return func.HttpResponse("No user specified")
 
@@ -14,6 +16,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     for report in result:
         report["report_success"] = report.get("report_success", False)
         report["plate_candidate_list"] = report.get("plate_candidate_list", [])
+    
+    if refresh_info:
+        for report in result:
+            enrich_report_info.enrich_report_info(report)
+            report_info_utils.save_report_info(report)
 
     return func.HttpResponse(
         json.dumps(result),
